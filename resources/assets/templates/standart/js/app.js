008af3c1337028fd04febd15b01f95515c6fe756
otjog <fvirus@mail.ru>
2019-04-04 23:25:58 +0300

    merge with npminit

---
+++ resources/assets/templates/standart/js/app.js
@@ -0,0 +1,5 @@
+require('./jquery-ui.min');
+require('@fancyapps/fancybox/dist/jquery.fancybox');
+require('suggestions-jquery/dist/js/jquery.suggestions.min');
+require('./custom');
+require('./product-filter');
---
+++ resources/assets/templates/standart/js/googlemap.js
@@ -0,0 +1,14 @@
+export default function InitMap(geo, zoom) {
+    let map = document.getElementById('map');
+
+    if(map !== undefined && map !== null){
+
+        let location = {lat: +geo.latitude, lng: +geo.longitude};
+
+        return new google.maps.Map(
+            map, {
+                zoom: zoom,
+                center: location
+            });
+    }
+}
