cb3c9ea36b8e86225c620b48944b78ad87401f47
otjog <fvirus@mail.ru>
2019-12-09 10:52:29 +0300

    Исправит баг вызова формы обратного звонка

--- resources/views/_kp/positions/modals/default.blade.php
+++ resources/views/_kp/positions/modals/default.blade.php
@@ -1,1 +1,2 @@
-@include($global_data['template']['name'] .'.positions.modals.forms.change-city')
+@include($global_data['template']['name']. '.positions.modals.forms.change-city')
+@include($global_data['template']['name']. '.positions.modals.forms.callback')
